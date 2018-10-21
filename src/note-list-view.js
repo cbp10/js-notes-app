fdb23517de81b7427af160976b86578781af1b38
Rashika Patel <rashika_patel@hotmail.com>
2018-10-21 01:52:19 +0100

    fix typo, close a tag

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -5,18 +5,18 @@
 
   NoteListView.prototype.getHTML = function() {
     var noteArray = [];
-    
+
     this.list.forEach(function(note) {
       var text = note.getNoteText()
-      var id = note.getNoteId() 
+      var id = note.getNoteId()
       var string = id + '">' + text
       if (text.length > 20) {
         string = id + '">' + text.slice(0, 19) + "..."
       }
       noteArray.push(string)
     })
-    
-    return '<ul><li><a href="#notes/' + noteArray.join('</li><li><a href="#notes/') + "</li></ul>"
+
+    return '<ul><li><a href="#notes/' + noteArray.join('</li><li><a href="#notes/>') + "</a></li></ul>"
   }
 
 
